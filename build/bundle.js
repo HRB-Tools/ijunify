af61f3df33c319cb99f9686e02fe3da38341297d
nilseidmann <nilseidmann@gmail.com>
2018-11-29 04:22:18 -0500

    Hard work pays off

--- build/bundle.js
+++ build/bundle.js
@@ -85,15 +85,12 @@
             return arr;
         }; // Klont Automatikkonten aus dem SKR-Objekt
         let a = auto(skr04); // Im Fall von SKR 03 entsprechend aendern!
-        //     console.log(a);
         for (let j = 0; j < a.length; j++) {
             if (parseInt(a[j]) == element[7] && element[8] == '"400"') {
-                console.log("Gegenkonto automatik, 400");
-                element[8] = "40";
+                element[8] = '"40"';
             }
             else if (parseInt(a[j]) == element[6] && element[8] == '"400"') {
-                console.log("Konto automatik, 400");
-                element[8] = "40";
+                element[8] = '"40"';
             }
         } // Entfernt den Schluessel bei Automatikkonten
         if (element[8] == '10' || element[8] == "10" || element[8] == '"10"') {
